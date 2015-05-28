2999a5c81d71cb57f6c454d606e74f041e472dcf
Jimmy Gong <gong.jim@gmail.com>
2015-05-27 22:50:27 -0700

    Solves problem 14 with dynamic programming solution

--- problem14/solver.js
+++ problem14/solver.js
@@ -1,24 +1,20 @@
 function solver(bound){
-  var res = {
-    num : 0,
-    steps: 0
-  }
   var memo = [0, 0];
+  var maxStepsIndex = 1;
   for (var i = 2; i < bound; i++){
-    var n = i, count = 0;
-    while (memo[n] === undefined){
-      if(n !== 1) {
-        count++;
-        if (n % 2) n = n * 3 + 1;
-        else n = n >> 1;
-      }
-    }
-    if (i === 13) debugger;
-    memo[i] = memo[n] + count;
-    if (count > res['steps']){
-      res['num'] = i;
-      res['steps'] = memo[i]; 
-    }
+    memo[i] = getStepsToOne(i);
+    if (memo[i] > memo[maxStepsIndex]) maxStepsIndex = i;
   }
-  return res['num'];
+
+  function getStepsToOne(n, steps){
+    var steps = 0;
+    while (n > 1){
+      if (memo[n]) return steps + memo[n];
+      n = n % 2 ? n*3+1 : n/2;
+      steps++;
+    }
+    return steps;
+  }
+
+  return maxStepsIndex;
 }
